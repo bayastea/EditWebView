9331d2f10d8fb41d1c52f4f3804ca68169bd7b31
bayastea <i.love.ama.frog@gmail.com>
2024-03-30 04:23:04 +0900

    ヘッダー非表示処理追加

--- app/src/main/java/jp/bayastea/editwebview/MainActivity.kt
+++ app/src/main/java/jp/bayastea/editwebview/MainActivity.kt
@@ -2,6 +2,8 @@ package jp.bayastea.editwebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import jp.bayastea.editwebview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        val webView = binding?.webview
+        webView?.settings?.javaScriptEnabled = true
+        webView?.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                // ヘッダーを非表示
+                view?.loadUrl(
+                    "javascript:(function() { " +
+                            "document.getElementsByClassName('devsite-top-logo-row-wrapper-wrapper')[0].style.display = 'none';" +
+                            "})()"
+                )
+            }
+        }
+
+        webView?.loadUrl("https://developer.android.com/?hl=ja")
     }
-
-
 }
